fb8513fccfbbf98628af666b674a683771a20bb6
Takuma <takuma012011@gmail.com>
2020-08-19 19:23:30 +0900

    Initial Commit

---
+++ TikTokApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TikTokApp
+//
+//  Created by 村上拓麻 on 2020/08/19.
+//  Copyright © 2020 村上拓麻. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
